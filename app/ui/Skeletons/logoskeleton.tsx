0e3f427e52341b7cff866de4d1dc1c2d21acb0ce
Shuriк <122827018+omykkru089@users.noreply.github.com>
2025-06-03 13:21:46 +0200

    Update logoskeleton.tsx

--- app/ui/Skeletons/logoskeleton.tsx
+++ app/ui/Skeletons/logoskeleton.tsx
@@ -12,7 +12,7 @@ const MyLoader = () => (
   foregroundColor="#999"
   uniqueKey="logo-skeleton"
 >
-  <rect x="0" y="30" rx="8" ry="8" width="100" height="40" />
+  <rect x="0" y="00" rx="8" ry="8" width="100" height="80" />
 </ContentLoader>
 );
 export default MyLoader
