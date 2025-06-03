70dbb8557fa227d3309d135bc39d935822e30a7b
omykkru089 <sasha20031218@gmail.com>
2025-06-03 12:42:13 +0200

    v0.05 boton pagar css

---
+++ app/ui/Skeletons/logoskeleton.tsx
@@ -0,0 +1,15 @@
+'use client'
+import React from "react"
+import dynamic from "next/dynamic";
+const ContentLoader = dynamic(() => import("react-content-loader"), { ssr: false });
+<ContentLoader 
+  speed={1}
+  width={120}
+  height={40}
+  viewBox="0 0 120 40"
+  backgroundColor="#333"
+  foregroundColor="#999"
+  uniqueKey="logo-skeleton"
+>
+  <rect x="0" y="0" rx="8" ry="8" width="120" height="40" />
+</ContentLoader>
---
+++ app/ui/Skeletons/searchnavskeleton.tsx
@@ -0,0 +1,19 @@
+'use client'
+import React from "react"
+import dynamic from "next/dynamic";
+const ContentLoader = dynamic(() => import("react-content-loader"), { ssr: false });
+<ContentLoader 
+  speed={1}
+  width={360}
+  height={45}
+  viewBox="0 0 360 45"
+  backgroundColor="#333"
+  foregroundColor="#999"
+  uniqueKey="search-nav-skeleton"
+>
+  {/* Lupa */}
+  <rect x="0" y="0" rx="8" ry="8" width="40" height="40" />
+
+  {/* Selector de plataformas */}
+  <rect x="50" y="0" rx="10" ry="10" width="310" height="40" />
+</ContentLoader>
